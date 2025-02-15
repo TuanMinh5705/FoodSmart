81cfd6aacae247da93b41d3b12a55f429716e51d
T14 <vykid9@gmail.com>
2025-02-15 12:04:49 +0700

    connectDB

--- src/main/java/com/example/foodSmart/dao/ConnectDB.java
+++ src/main/java/com/example/foodSmart/dao/ConnectDB.java
@@ -5,6 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/foodsmart";
+    private static final String USER = System.getenv("DB_USERNAME") ;
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
+    // Debug
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "1209";
+
+    public static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Kết nối thành công!");
+            return conn;
+        } catch (SQLException e) {
+            System.err.println("Lỗi kết nối database: " + e.getMessage());
+            return null;
+        }
+    }
 }
 
