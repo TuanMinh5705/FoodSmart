8cad76a3de1d2908853cd1abcb4d782d982002e4
Duc-khanh <nguyenkhanh2561990@gmail.com>
2025-02-18 09:06:03 +0700

    update

--- src/main/java/com/example/foodSmart/controller/ManageMerchants.java
+++ src/main/java/com/example/foodSmart/controller/ManageMerchants.java
@@ -5,14 +5,17 @@ import com.example.foodSmart.service.IMerchantService;
 import com.example.foodSmart.service.MerchantService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/manageMerchants")
+@MultipartConfig
 public class ManageMerchants extends HttpServlet {
     IMerchantService merchantService = new MerchantService();
 
@@ -31,12 +34,22 @@ public class ManageMerchants extends HttpServlet {
                 break;
             case "updateMerchantForm":
                 int store_id = Integer.parseInt(req.getParameter("store_id"));
+                req.setAttribute("merchant", merchantService.getMerchantById(store_id));
                 req.getRequestDispatcher("view/admin/homeAdmin.jsp?page=updateMerchants").forward(req, resp);
+                break;
+            case "Detail":
+                showMerchantDetails(req, resp);
                 break;
             default:
                 listMerchant(req, resp);
                 break;
         }
+    }
+
+    private void showMerchantDetails(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int store_id = Integer.parseInt(req.getParameter("store_id"));
+        req.setAttribute("merchant", merchantService.getMerchantById(store_id));
+        req.getRequestDispatcher("view/admin/homeAdmin.jsp?page=merchantDetails").forward(req, resp);
     }
 
     private void listMerchant(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -69,8 +82,14 @@ public class ManageMerchants extends HttpServlet {
         String store_name = req.getParameter("store_name");
         String store_address = req.getParameter("store_address");
         String contact_number = req.getParameter("contact_number");
-        String banner_path = req.getParameter("banner_path");
-        String avt_path = req.getParameter("avt_path");
+
+
+        Part fileBannerPart = req.getPart("banner_path");
+        String banner_path = (fileBannerPart != null && fileBannerPart.getSize() > 0) ? fileBannerPart.getSubmittedFileName() : req.getParameter("current_banner_path");;
+
+        Part fileAvatarPart = req.getPart("avt_path");
+        String avt_path = (fileAvatarPart != null && fileAvatarPart.getSize() > 0) ? fileAvatarPart.getSubmittedFileName() : req.getParameter("current_avt_path");;
+
         boolean store_type = Boolean.parseBoolean(req.getParameter("store_type"));
         Merchant merchant = new Merchant(store_id ,store_name, store_address, contact_number, banner_path, avt_path, store_type);
         merchantService.updateMerchant(merchant);
